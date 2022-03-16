8052299a5e54a3e83d9d710cf4fbbb28df6c85b0
dixit joshi <44132789+DixitJoshi12@users.noreply.github.com>
2022-03-17 02:20:28 +0530

    form validation
    day 4 form validation

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,8 +1,60 @@
-// import { useState } from "react";
+import { useState } from "react";
 import classes from "./components-styles/LoginForm.module.css";
 
 const LoginForm = () => {
-  // useState(false);
+  const [enteredName, setEnteredName] = useState("");
+  const [enteredPassword, setEnteredPassword] = useState("");
+
+  const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+  const [enteredPasswordTouched, setEnteredPasswordTouched] = useState(false);
+
+  const enteredNameValid = enteredName.trim() !== "";
+  const enteredNameInvalid = !enteredNameValid && enteredNameTouched;
+
+  const enteredPasswordValid = enteredPassword.trim() !== "";
+  const enteredPasswordInvalid = !enteredPasswordValid && enteredPasswordTouched;
+
+  let formIsValid = false;
+  if(enteredNameValid && enteredPasswordValid){
+    formIsValid = true;
+  }
+
+  const nameChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const passwordChangeHandler = (event) => {
+    setEnteredPassword(event.target.value);
+  };
+
+  const nameBlurHandler = (event) => {
+    setEnteredNameTouched(true);
+  };
+
+  const passwordBlurHandler = (event) => {
+    setEnteredPasswordTouched(true);
+  };
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+
+    setEnteredNameTouched(true);
+    setEnteredPasswordTouched(true);
+    if(!enteredNameValid && !enteredPasswordValid){
+      return;
+    }
+    if (!enteredNameValid) {
+      return;
+    }
+    if (!enteredPasswordValid) {
+      return;
+    }
+
+    setEnteredName("");
+    setEnteredPassword("");
+    setEnteredNameTouched(false);
+    setEnteredPasswordTouched(false);
+  };
   return (
     <div className={classes["inner-box"]}>
       <div className={classes.upper}>
@@ -12,18 +64,31 @@ const LoginForm = () => {
         />
         <h3>Login</h3>
       </div>
-      <form action="">
-        <input name="user" type="text" placeholder="username" />
-        <p>Username must be at least 5 characters.</p>
-        <input name="password" type="password" placeholder="password"/>
-        <p>Password must be at least 6 characters.</p>
-        <button disabled className={classes.btn1}>
-          Submit
-        </button>
+      <form onSubmit={formSubmitHandler}>
+        <input
+          name="user"
+          type="text"
+          onChange={nameChangeHandler}
+          onBlur={nameBlurHandler}
+          placeholder="username"
+          value={enteredName}
+        />
+        {enteredNameInvalid && <p>Username must be at least 5 characters.</p>}
+        <input
+          name="password"
+          type="password"
+          onChange={passwordChangeHandler}
+          onBlur={passwordBlurHandler}
+          placeholder="password"
+          value={enteredPassword}
+        />
+        {enteredPasswordInvalid && (
+          <p>Password must be at least 6 characters.</p>
+        )}
+        <button disabled={!formIsValid} className={classes.btn1}>Submit</button>
       </form>
     </div>
   );
 };
 
 export default LoginForm;
-
